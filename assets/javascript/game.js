9596d53bece5608a99566a87b968ed3438a9d5d5
Tanya Valdez <TValdez@digmap.com>
2018-08-12 23:12:46 -0700

    update functions in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,23 +4,26 @@ const letters = [
   'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
+// variables within the game
 let wins = 0
 let losses = 0
 let guessedLetters = []
 let guessesLeft = 10
-let compChoice = compGuess()
+let compChoice = ' '
+gameState = true
 
-// keyboad event funtion capturing the letters entered by the player
-document.onkeyup = function (event) {
-  const letter = event.key 
-  guessedLetters.push(letter)
-// passes letters guessed to the html page
-  document.querySelector('#guesses').innerHTML = guessedLetters.toString()
- 
-  // is letter guessed same as comp choice? if yes, win; otherwise check to see if guessesLeft >0 if not wrong and goes down by one
-  letter === compChoice? win() : (guessesLeft > 0 ?  wrong () : lose ())
-
+// function to reset the game
+function reset () {
+  gameState = true
+  compChoice = compGuess()
+  guessesLeft = 10
+  guessedLetters = []
+   document.querySelector('#guessesLeft').innerHTML = ' ' + guessesLeft
+   document.querySelector('#wins').innerHTML = ' ' + wins
+   document.querySelector('#losses').innerHTML = ' ' + losses
+   document.querySelector('#guesses').innerHTML = ' ' + guessedLetters.toString()
 }
+
 
 // funtion to get the computers random guess via the letters in the array
  function compGuess () {
@@ -30,6 +33,8 @@ document.onkeyup = function (event) {
 // function for if player wins
   function win () {
     wins++
+    gameState = false
+    document.querySelector('#wins').innerHTML = ' ' + wins
 
 
   }
@@ -37,25 +42,59 @@ document.onkeyup = function (event) {
 // function for if player loses
   function lose () {
   losses++
+  gameState = false
+  document.querySelector('#losses').innerHTML = ' ' + losses
+  document.querySelector('#guessesLeft').innerHTML = ' ' + guessesLeft
 
   }
 
-// function for if player is wrong
+// function for if player is wrong which then passes the guessesLeft to the html page
   function wrong () {
-     guessesLeft--
-
+   document.querySelector('#guessesLeft').innerHTML = ' ' + guessesLeft
   }
 
+  // function to make sure only letters in the array are pressed
+  function alphaCheck (letter) {
+    let result = false
 
+    for (let i = 0; i < letters.length; i++) {
+      if  (letter.toLowerCase() === letters[i]) {
+        if (gameState === true) {
+            return guessedAlready(letter)
+        } else {
+          return false
+        }
+      } 
+    }
+    return result
+  }
 
+  // function so that the player cannot press the same letter in the game
+  function guessedAlready (letter) {
+    let result = true
+    for (let i = 0; i < guessedLetters.length; i++) {
+      if (letter === guessedLetters[i]) {
+        return false
+      }
+    }
+        return result
+  }
 
+// keyboad event funtion capturing the letters entered by the player and passes letters guessed to the html page
+document.onkeyup = function (event) {
+  const letter = event.key
+  if (letter != 'Enter') {
+    if  (alphaCheck(letter)) {
+       guessedLetters.push(letter) 
+        document.querySelector('#guesses').innerHTML = guessedLetters.toString()
+        guessesLeft--
+  
+  // is letter guessed same as comp choice? if yes, win; otherwise check to see if guessesLeft >0 if not wrong and goes down by one
+  letter === compChoice ? win() : (guessesLeft > 0 ? wrong() : lose(letter))
+} 
+}else {
+     reset()
+}
+}
 
-// for(i=1; i<10; i++) {
-//   console.log(i);
-// }
-
-
-
-// function randLetters() {
-//   document.getElementById("GuessSoFar");
-// }
+reset()
