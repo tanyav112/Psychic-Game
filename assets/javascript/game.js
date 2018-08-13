66b8710448221c12ef51a1b1da6845beb97a4ab6
Tanya Valdez <TValdez@digmap.com>
2018-08-12 20:59:44 -0700

    update index.html and add functions to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,60 @@
-const letterArr = [
+//array of letters to be used in the game
+const letters = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
   'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
-for(i=1; i<10; i++) {
-  console.log(i);
+let wins = 0
+let losses = 0
+let guessedLetters = []
+let guessesLeft = 10
+let compChoice = compGuess()
+
+// keyboad event funtion capturing the letters entered by the player
+document.onkeyup = function (event) {
+  const letter = event.key 
+  guessedLetters.push(letter)
+  document.querySelector('#guesses').innerHTML = guessedLetters.toString()
+ 
+  // is letter guessed same as comp choice? if yes, win; otherwise check to see if guessesLeft >0 if not wrong and goes down by one
+  letter === compChoice? win() : (guessesLeft > 0 ?  wrong () : lose ())
+
 }
 
-//pick random value from this array//
-function randLetters () {
-  let randLetters = letterArr[Math.floor(Math.random()) * (letterArr.length)];
-    return randLetters;
-    console.log(randLetters)
-}
+// funtion to get the computers random guess via the letters in the array
+ function compGuess () {
+   return letters[Math.floor(Math.random() * letters.length)]
+   }
 
-function randLetters() {
-  document.getElementById("GuessSoFar");
-}
+// function for if player wins
+  function win () {
+    wins++
+
+
+  }
+
+// function for if player loses
+  function lose () {
+  losses++
+
+  }
+
+// function for if player is wrong
+  function wrong () {
+     guessesLeft--
+
+  }
+
+
+
+
+
+// for(i=1; i<10; i++) {
+//   console.log(i);
+// }
+
+
+
+// function randLetters() {
+//   document.getElementById("GuessSoFar");
+// }
